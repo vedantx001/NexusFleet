49eb10376ba40e0c1a3cbe60577ac3764a90ac05
Vraj Patel <vrajrpatel6261@gmail.com>
2026-02-21 13:42:43 +0530

    Schema added

---
+++ server/src/models/FuelLog.js
@@ -0,0 +1,22 @@
+const fuelLogSchema = new mongoose.Schema({
+
+  vehicle: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Vehicle",
+    required: true
+  },
+
+  trip: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Trip"
+  },
+
+  liters: { type: Number, required: true },
+
+  cost: { type: Number, required: true },
+
+  date: { type: Date, default: Date.now }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("FuelLog", fuelLogSchema);
